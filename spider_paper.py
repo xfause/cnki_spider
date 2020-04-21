a5f54d45997d4091c16dfd418fc430ebdd0fa49e
xfause <919017553@qq.com>
2020-04-22 04:39:30 +0800

    feat(issue#1)

--- spider_paper.py
+++ spider_paper.py
@@ -12,20 +12,39 @@ if __name__ == "__main__":
 	#tags=soup.find_all('a')
 	file = open("data.txt")
 	lines = file.readlines()
-	txt_num=1;
+	txt_num=1
 	for line in lines:
-		file_name='./data/out_'+str(txt_num)+'.txt'
 		paper_url = line
+		temp = paper_url.split('/')
+		file_name = "./data/" + temp[len(temp)-1] + '.txt'
 		html = urllib.request.urlopen(paper_url).read()
 		soup = BeautifulSoup(html,'html.parser')
+		file_content = open(file_name,'a')
+
+		file_content.write("Title:")
+		paper_title = soup.find_all('h1',class_="xx_title")
+		for thing in paper_title:
+			a = thing.strings
+			for string in a:
+				file_content.write(string)
+
+		file_content.write("\nAuthors:")
+		paper_authors = soup.find_all('div',style="text-align:center; width:740px; height:30px;")
+		for paper_author in paper_authors:
+			names = paper_author.find_all('a',target='_blank')
+			for name in names:
+				a = name.strings
+				for string in a:
+					file_content.write(string + ' ')
+		
+		file_content.write("\nAbstracts:")
 		pp=soup.find_all('div',style="text-align:left;word-break:break-all")
-		fuck = open(file_name,'a')
 		for thing in pp:
 			a = thing.strings
 			for string in a:
-				fuck.write(string)
+				file_content.write(string)
 			txt_num+=1
-		fuck.close()
+		file_content.close()
 		#[p.next_sibling.strip() for p in soup.find_all('p')]
 	file.close()
 	end = time.clock()
